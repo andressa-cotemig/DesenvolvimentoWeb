c672eca0ef1c5c3b01abb402bab971b3064c470b
Andressa Oliveira <C0614396@vale.com>
2021-10-05 18:08:51 -0300

    att

--- React/src/aula06_lifecycle/Aula06.js
+++ React/src/aula06_lifecycle/Aula06.js
@@ -28,6 +28,12 @@ export default class Aula06 extends React.Component {
     return (
       <div className="Aula06">
         <div className="item">
+          <Header favcol="blue" />
+        </div>
+        <div className="item">
+          <Container />
+        </div>
+        <div className="item">
           <button onClick={() => this.click()}>Clique aqui</button>
           <button onClick={() => this.removeComponente()}>Remove componente</button>
           {this.state.lifecyle && <LifeCycle exibicao={this.state.exibe} />}
