ac5f93cbabff9e4ac80fe0df7a6aea91e9dad9c9
Andressa Oliveira <C0614396@vale.com>
2021-10-05 20:14:44 -0300

    att

--- React/src/aula06_lifecycle/Header/Header.js
+++ React/src/aula06_lifecycle/Header/Header.js
@@ -3,25 +3,25 @@ import React from 'react';
 export default class Header extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { favoritecolor: "red" };
+        this.state = { favoritecolor: "pink" };
     }
-    static getDerivedStateFromProps(props, state) {
-        return { favoritecolor: props.favcol };
-    }
+    // static getDerivedStateFromProps(props, state) {
+    //     return { favoritecolor: props.favcol };
+    // }
     componentDidMount() {
         setTimeout(() => {
-            this.setState({ favoritecolor: "yellow" })
-        }, 1000)
+            this.setState({ favoritecolor: "blue" })
+        }, 5000)
     }
 
-    // getSnapshotBeforeUpdate(prevProps, prevState) {
-    //     document.getElementById("div1").innerHTML =
-    //         "Antes da atualização, o favorito era " + prevState.favoritecolor;
-    // }
-    // componentDidUpdate() {
-    //     document.getElementById("div2").innerHTML =
-    //         "O favorito atualizado é " + this.state.favoritecolor;
-    // }
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        document.getElementById("div1").innerHTML =
+            "Antes da atualização, o favorito era " + prevState.favoritecolor;
+    }
+    componentDidUpdate() {
+        document.getElementById("div2").innerHTML =
+            "O favorito atualizado é " + this.state.favoritecolor;
+    }
 
     render() {
         return (
--- React/src/aula06_lifecycle/Lifecycle/Lifecycle.js
+++ React/src/aula06_lifecycle/Lifecycle/Lifecycle.js
@@ -1,20 +1,20 @@
 import React from 'react';
 export class LifeCycle extends React.Component {
     componentDidMount() {
-        console.log('Componente montado', this.props)
+        console.log('Componente Lifecycle montado', this.props)
     }
     componentDidUpdate() {
-        console.log('Componente atualizado', this.props)
+        console.log('Componente Lifecycle atualizado', this.props)
     }
     componentWillUnmount() {
-        console.log('Componente desmontado', this.props)
+        console.log('Componente Lifecycle desmontado', this.props)
     }
     render() {
         return (
-            <div className="App" >
+            <div className="Lifecycle">
                 <h1>Componente Lifecycle</h1>
                 <br />
-                {this.props.exibicao ? <h2>Exibindo...</h2> : null}
+                {this.props.exibicao ? <h2>Exibindo nome: Andressa</h2> : null}
             </div>
         );
     }
